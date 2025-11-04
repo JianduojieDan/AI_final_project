5dbf9641b029041557fe223a9def38e098a42c7a
Zhuanz <dj12241@auca.kg>
2025-11-04 22:20:20 +0600

    finish G01 G33 and G62 script refactor and set up a good work flow

---
+++ script_to_clean_data/get_name/G62.py
@@ -0,0 +1,30 @@
+import geopandas as  gpd
+
+gpkg_file_path = '/Users/Zhuanz/Documents/ACADEMIC/fifth-semaster/Introduction-to-AI/final_project/data/Geopackage_2021_G62_NSW_GDA2020/G62_NSW_GDA2020.gpkg'
+DATA_LAYER_NAME = 'G62_SA1_2021_NSW'
+
+print(f"--- Start exploring file: {gpkg_file_path} ---")
+print(f"--- Target Layer: {DATA_LAYER_NAME} ---")
+
+try:
+    gdf = gpd.read_file(gpkg_file_path,layer=DATA_LAYER_NAME,engine='pyogrio')
+
+    all_columns = gdf.columns.tolist()
+    print(f"\nFile Read Successful! Total Columns: {len(all_columns)} ---")
+
+    print("\nColumn Headers (Row Names) List:")
+    for col in all_columns:
+        print(f" - {col}")
+    print("\n-------------------")
+
+    #geographic data
+    id_col = next((col for col in all_columns if 'SA1_CODE' in col), 'SA1_ID_NOT_FOUND')
+    print(f"Found Geographic ID Header: {id_col}")
+
+    preview_cols = [id_col, 'geometry'] + [c for c in all_columns if c not in [id_col, 'geometry']][:4]
+    print("\nPreview of First 5 Rows (ID, Geometry, and First 4 Data Cols):")
+    print(gdf[preview_cols].head())
+
+
+except:
+    print(f"--- Error: Cannot read file: {gpkg_file_path} ---")
